a4f58b8292d7374a8b89602e3a403fd74577f8a1
ctwoolsey <clark.woolsey@gmail.com>
2023-07-18 15:52:48 +0200

    documented the range filter used

--- include/constants.php
+++ include/constants.php
@@ -8,4 +8,12 @@ const SERVER = "aprs.glidernet.org";
 const PORT = 14580;
 const MYCALL = "GEZC0";
 const PASSCODE = -1;
+/*
+http://www.aprs-is.net/javAPRSFilter.aspx  for filter information.  This filter is the range filter.
+r/lat/lon/dist
+Pass posits and objects within dist km from lat/lon.
+lat and lon are signed decimal degrees, i.e. negative for West/South and positive for East/North.
+*/
 const FILTER = "r/52.06/5.94/50";
+
+
