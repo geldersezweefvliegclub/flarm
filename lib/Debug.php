6604ab39ffa047888b5423f304c2132a634f907c
rjnkr <richard@jnkr.eu>
2024-07-20 20:53:26 +0200

    bugje, date is geen class :-(

--- lib/Debug.php
+++ lib/Debug.php
@@ -4,7 +4,7 @@ include_once 'include/config.php';
 class Debug {
   public function echo(string $str, bool $always_show=false): void {
     if(DEBUG === true || $always_show) {
-      echo new date("Y-m-d H:i:s") . "| $str\n";
+      echo date("Y-m-d H:i:s") . "| $str\n";
     }
   }
 
