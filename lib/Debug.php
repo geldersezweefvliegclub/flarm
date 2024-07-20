fc4fb2c80608e6926de167986ba04c0f6aeaff38
rjnkr <richard@jnkr.eu>
2024-07-20 17:45:28 +0200

    timestamp in debug messages

--- lib/Debug.php
+++ lib/Debug.php
@@ -4,7 +4,7 @@ include_once 'include/config.php';
 class Debug {
   public function echo(string $str, bool $always_show=false): void {
     if(DEBUG === true || $always_show) {
-      echo "$str\n";
+      echo new date("Y-m-d H:i:s") . "| $str\n";
     }
   }
 
